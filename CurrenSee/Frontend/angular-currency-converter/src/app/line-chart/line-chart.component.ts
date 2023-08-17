8f69aca28a55868f0d4d645eec8eb6765439f9eb
Bill <wyze97@gmail.com>
2023-08-17 01:36:06 +0100

    styling

--- CurrenSee/Frontend/angular-currency-converter/src/app/line-chart/line-chart.component.ts
+++ CurrenSee/Frontend/angular-currency-converter/src/app/line-chart/line-chart.component.ts
@@ -50,21 +50,21 @@ export class LineChartComponent implements OnInit {
 								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
 	       datasets: [
           {
-            label: "Sales",
+            label: "Rates",
             data: ['467','576', '572', '79', '92',
 								 '574', '573', '576'],
-            backgroundColor: 'blue'
+            backgroundColor: 'white'
           },
-          {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
+          // {
+          //   label: "Profit",
+          //   data: ['542', '542', '536', '327', '17',
+					// 				 '0.00', '538', '541'],
+          //   backgroundColor: 'limegreen'
+          // }  
         ]
       },
       options: {
-        aspectRatio:2.5
+        aspectRatio:1.4
       }
       
     });
